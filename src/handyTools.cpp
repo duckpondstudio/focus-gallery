3219d9a10401c6520d298a377b19a9fc97a0874f
Nick Yonge <nick@curiobot.com>
2023-08-21 01:38:07 -0700

    wee lil script to hold useful rando tidbits

---
+++ src/handyTools.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void ReadByte(byte b) {
+    Serial.print("Byte: ");
+    for (int i = 0; i < 8; i++)
+    {
+        Serial.print(bitRead(b, i));
+    }
+    Serial.print(" = ");
+    Serial.println(b);
+}
